360a8af56951741e2dc18fea4d944e963befe6bc
Sergio Camacho <sergio.camachoo@misena.edu.co>
2021-11-20 21:41:02 -0500

    Avances Tema 4

---
+++ src/views/Tema4.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 4
+      h1 Maqueta de sonido
+
+    p.mb-5 En el siguiente video se expone el proceso básico para el montaje de los cuadros elaborados en el proceso de storyboard con la finalidad de elaborar una animatic.Para esto se utilizará el programa Blender el cual es un software libre que se puede obtener gratuitamente en la página www.blender.org.
+
+    h3.titulo-tercero Instalación de Blender
+
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+    p.mb-5 Aunque su uso principalmente está enfocado en el 3D, posee un apartado dedicado a la edición de video básica, ideal para introducirse en el mundo del montaje.
+
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+    p.mb-5 Por otro lado, se pone a disposición una serie de videos que presenta el software especializado StoryboardPro y el proceso que se debe seguir para la construcción de un animatic.
+
+    .row.justify-content-center
+      .col-lg-5
+        a.anexo.color-acento-contenido.mb-4.mb-lg-0(href="https://en.wikipedia.org/wiki/Main_Page" target="_blank")
+          .anexo__icono
+            img(src="@/assets/curso/link-amarillo.svg")
+          .anexo__texto
+            p
+              strong Enlace.
+              i   Webinar - Learn How to Create Animatics in Storyboard Pro
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema3',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
